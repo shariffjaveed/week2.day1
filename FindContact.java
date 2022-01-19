675161b8c47586a50fa5d966aab5d1ce708a0d73
Javeed Shariff <96673407+shariffjaveed@users.noreply.github.com>
2022-01-19 14:20:50 +0530

    Add files via upload

---
+++ FindContact.java
@@ -0,0 +1,30 @@
+package week2.day1;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class FindContact {
+
+	public static void main(String[] args) 
+	{
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps/");
+		driver.manage().window().maximize();
+		driver.findElement(By.id("username")).sendKeys("DemoCSR");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Contacts")).click();
+		driver.findElement(By.linkText("Find Contacts")).click();
+		driver.findElement(By.linkText("Email")).click();
+		driver.findElement(By.name("emailAddress")).sendKeys("babu@testleaf.com");
+		driver.findElement(By.xpath("//button[text()='Find Contacts']")).click();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3000));
+		driver.close();
+
+}
+}
